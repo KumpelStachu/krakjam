b181ac58e47b8f750c5665baf4a3f2362397d0e5
KumpelStachu <stanislaw.rzeszut@gmail.com>
2024-01-28 00:58:37 +0100

    takjakjest

--- Assets/Scripts/CircleScript.cs
+++ Assets/Scripts/CircleScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -13,15 +14,38 @@ public class CircleScript : MonoBehaviour
 
         _clicked = true;
         GameManager.instance.AddPoints(10);
+        GameManager.instance.Streak(true);
         var animator = GetComponent<Animator>();
         animator.speed *= speedMultiplier;
     }
 
-    public void Die()
+    private void OnDestroy()
     {
-        transform.GetComponentInChildren<ParticleSystem>().Play();
-        transform.DetachChildren();
+        if (!_clicked)
+            GameManager.instance.Streak(false);
+    }
 
+    public void KillMe() => StartCoroutine(nameof(Die));
+
+    public IEnumerator Die()
+    {
+        yield break;
+        if (!_clicked) yield break;
+
+        var child = transform.GetChild(0);
+        var particle = child.GetComponent<ParticleSystem>();
+        // particle.playOnAwake = true;
+        // child.gameObject.SetActive(true);
+        particle.time = 0;
+        particle.Play();
+
+        var trans = transform;
+        trans.DetachChildren();
+        trans.position = new Vector3(69, 69);
+
+        yield return new WaitForSecondsRealtime(1);
+
+        Destroy(child.gameObject);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -7,10 +8,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Spawner[] spawners;
     [SerializeField] private Sprite[] hpSprites;
     [SerializeField] private Image hpImage;
+    [SerializeField] private Sprite[] kingSprites;
+    [SerializeField] private SpriteRenderer kingRenderer;
+    [SerializeField] private Transform kingContainer;
     [SerializeField] private TMP_Text pointsText;
     [SerializeField] private int health;
 
-    [SerializeField] private int points;
+    public int points;
+    public int streak;
+    public int kingSize = 1;
 
     private static GameManager _Instance;
 
@@ -42,11 +48,37 @@ public class GameManager : MonoBehaviour
 
         return _Instance;
     }
+
     public void AddPoints(int amount)
     {
         points += amount;
         pointsText.text = points.ToString();
         pointsText.GetComponent<Animator>().Play("PointsAddPop");
+    }
 
+    public void Streak(bool ok)
+    {
+        if (streak > 0)
+            streak = ok ? streak + 1 : 0;
+        else
+            streak += ok ? 1 : -1;
+
+        if (Math.Abs(streak) >= 10)
+        {
+            kingSize += streak / 10;
+            streak = 0;
+        }
+
+        if (kingSize >= kingSprites.Length)
+            Debug.Log("Win");
+        else if (kingSize < 0)
+            Debug.Log("GameOver");
+        else
+        {
+            var delta = new Vector3(0, (kingSize - 1) * 0.03f);
+            kingContainer.localScale = Vector3.one + delta;
+            kingContainer.localPosition = delta * 6;
+            kingRenderer.sprite = kingSprites[kingSize];
+        }
     }
 }
